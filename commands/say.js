2f838256a3bc433e40eddc40fab9e42c95a1782a
Nicolas <52798064+nicolasaj0@users.noreply.github.com>
2023-12-12 15:15:38 -0300

    Delete commands directory

--- commands/say.js
+++
@@ -1,9 +0,0 @@
-//Comando que faz com que o bot fale o que você desejar, apagando logo em seguida a mensagem do comando.
-
-const Discord  = require("discord.js");
-
-module.exports.run = async (client, message, args) => {
-  const sayMessage = args.join(" ");
-  message.delete().catch(O_o => {});
-  message.channel.send(sayMessage);
-};
